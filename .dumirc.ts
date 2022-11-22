1477e595a673446bfa9c4ed1b54f3388d2209e9c
gong <517441540@qq.com>
2022-11-23 01:11:01 +0800

    chore: docs github page

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: process.env.NODE_ENV === 'production' ? `/marketing-components/` : '/',
   publicPath:
     process.env.NODE_ENV === 'production' ? `/marketing-components/` : '/',
 });
