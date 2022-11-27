c29b8f6f787455478785e543a851f6f9488103f6
gong <517441540@qq.com>
2022-11-27 23:04:57 +0800

    docs: adjust doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  themeConfig: {
+    name: 'mcp',
+    nav: [
+      {
+        title: '介绍',
+        link: '/guide',
+      },
+      {
+        title: '组件',
+        link: '/components/lottery',
+      },
+    ],
+  },
   outputPath: 'docs-dist',
   base: process.env.NODE_ENV === 'production' ? `/marketing-components/` : '/',
   publicPath:
